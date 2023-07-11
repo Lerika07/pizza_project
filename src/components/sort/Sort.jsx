0046787bf86a9c522d81e3a7645fc24aacf389c8
Valeria <lerika71299@gmail.com>
2023-07-11 23:29:52 +0300

    sort add react, fixed scss

--- src/components/sort/Sort.jsx
+++ src/components/sort/Sort.jsx
@@ -1,16 +1,35 @@
+import { useState } from "react";
 import "./sort.scss";
 
 const Sort = () => {
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState(0);
+  const sortList = ["популярності", "ціні", "алфавіту"];
+  const sortName = sortList[selected];
+
+  const onActive = (i) => {
+    setSelected(i);
+    setOpen(false);
+  };
+
   return (
     <div className="sort">
-      Сортування по: <span>популярності</span>
-      <div className="sort__popup">
-        <ul>
-          <li className="active">популярності</li>
-          <li>ціні</li>
-          <li>алфавіту</li>
-        </ul>
-      </div>
+      Сортування по: <b onClick={() => setOpen(!open)}>{sortName}</b>
+      {open && (
+        <div className="sort__popup">
+          <ul>
+            {sortList.map((item, i) => (
+              <li
+                key={i}
+                onClick={() => onActive(i)}
+                className={selected === i ? "active" : ""}
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
