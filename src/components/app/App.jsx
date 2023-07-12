5672aea5a68c7af277f20d80ec3477dacdc9ae22
Valeria <lerika71299@gmail.com>
2023-07-12 22:45:17 +0300

    add axios

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 import Categories from "../categories/Categories";
 import Header from "../header/Header";
 import Pizza from "../pizza-block/Pizza";
 import Sort from "../sort/Sort";
-import pizzas from "../test/pizzas.json";
+import axios from "axios";
+import { PulseLoader } from "react-spinners";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const _url = "https://62e91da601787ec7121197b8.mockapi.io/items";
+
+  useEffect(() => {
+    axios
+      .get(_url)
+      .then((data) => setItems(data.data))
+      .finally(setIsLoading(false));
+  }, []);
+
   return (
     <div className="app">
       <Header />
@@ -15,9 +29,11 @@ function App() {
           <Sort />
         </div>
         <div className="pizzas container">
-          {pizzas.map((obj) => (
-            <Pizza key={obj.id} {...obj} />
-          ))}
+          {isLoading ? (
+            <PulseLoader className="loading" color="#36d7b7" />
+          ) : (
+            items.map((obj) => <Pizza key={obj.id} {...obj} />)
+          )}
         </div>
       </main>
       <footer className="footer"></footer>
